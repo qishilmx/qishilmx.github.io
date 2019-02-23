28aae0debe110fe80e7a41bf93ef25e33611f118
尧无郁 <qishilmxcv@outlook.com>
2019-02-23 11:11:10 +0800

    Site updated: 2019-02-23 11:11:05

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Feb 23 2019 10:40:31 GMT+0800 (GMT+08:00)
+// build time:Sat Feb 23 2019 11:10:50 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
