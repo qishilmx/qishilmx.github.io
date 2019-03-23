741c7d5bd6052a207c3de2999ad8fa89e7a4a203
尧无郁 <qishilmxcv@outlook.com>
2019-03-23 23:51:08 +0800

    Site updated: 2019-03-23 23:50:51

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 19 2019 22:27:57 GMT+0800 (GMT+08:00)
+// build time:Sat Mar 23 2019 23:50:40 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
